ccccdd72b77f21e7b8b340209c5bb17e12930f43
Justin Kahrs <justin@justinkahrs.com>
2025-02-11 00:37:34 -0500

    Update ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -41,7 +41,7 @@ export default function ProductCard({
   direction = "left",
 }: ProductCardProps) {
   const Icon = product.icon;
-  const ref = useRef(null);
+  const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["0.8 1", "1 1"],
