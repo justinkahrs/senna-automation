a229c7eaab1f663ef2d0bfe89886473e23bf5f5f
Justin Kahrs <justin@justinkahrs.com>
2026-07-09 11:02:49 -0400

    consolidated local seo routes and added pr checks

---
+++ src/components/app/routes/LocalSeoRouteClient.tsx
@@ -0,0 +1,4 @@
+import LocalSeoPage from "@/components/localSeo/LocalSeoPage";
+import { createRoutedPage } from "@/components/app/createRoutedPage";
+
+export default createRoutedPage(LocalSeoPage);
